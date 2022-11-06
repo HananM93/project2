8ef3352ba5336eaf6e9cf919e67fdbe2eb497515
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2022-11-06 05:12:54 -0600

    updated foodSeed.js file

---
+++ models/foodSeed.js
@@ -0,0 +1,56 @@
+module.exports = [
+{
+  name: 'Bariis iyo Suqaar',
+  price: 13.99,
+  Image: f,
+  Description: [
+    "Beef Suqaar: cubes of beef cooked with vegetables and spices",
+    "Rice: basmati rice cooked with onions, garlic, and spices",
+    ]
+},
+{
+  name: 'Baasto iyo Suugo',
+  price: 12.99,
+  Image: f,
+  Description: [
+    "Beef Suqaar: cubes of beef cooked with vegetables and spices",
+    "Pasta: thin spaghetti made with tomato sauce, spices, and cheese",
+    ]
+  },
+{
+  name: 'Chicken Suqaar & Malawax',
+  price: 10.99,
+  Image: f,
+  Description: [
+    "Chicken Suqaar: cubes of chicken breast cooked with vegetables and spices",
+    "Malawax: thin bread similar to pancakes & crepes",
+    ]
+  },
+{
+  name: 'Bariis iyo Chicken Suqaar',
+  price: 11.99,
+  Image: f,
+  Description: [
+    "Chicken Suqaar: cubes of chicken breast cooked with vegetables and spices",
+    "Rice: basmati rice cooked with onions, garlic, and spices",
+    ]
+ },
+{
+  name: 'Canjeero iyo Beer',
+  price: 15.99,
+  Image: f,
+  Description: [
+    "Beer: thin strips of goat liver cooked with vegetables and spices",
+    "Canjeero: thin bread similar to pancakes & crepes",
+    ]
+ },
+{
+  name: 'Hilib iyo Beer',
+  price: 14.99,
+  Image: f,
+  Description: [
+      "Goat: medium cut pieces of goat boiled to tender and sautéed with spices & vegetables",
+      "Rice: basmati rice cooked with onions, garlic, and spices",
+      ]
+   },
+]
